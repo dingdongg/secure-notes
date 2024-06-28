80a4ec87ba8df76a80520649e7841dbce459fc85
dingdongg <cs.dg.kim@gmail.com>
2024-06-28 16:00:58 -0600

    calculate cursor position at runtime

--- tedit.c
+++ tedit.c
@@ -89,11 +89,31 @@ char editorReadKey() {
     return c;
 }
 
+int getCursorPosition(int *rows, int *cols) {
+    if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
+
+    printf("\r\n");
+    char c;
+    while (read(STDIN_FILENO, &c, 1) == 1) {
+        if (iscntrl(c)) {
+            printf("%d\r\n", c);
+        } else {
+            printf("%d ('%c')\r\n", c, c);
+        }
+    }
+
+    editorReadKey();
+    return -1;
+}
+
 int getWindowSize(int *rows, int *cols) {
     struct winsize ws;
 
-    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
-        return -1;
+    if (1 || ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+        // workaround for systems that don't allow window size requests
+        // move cursor to bottom right, then query its current position
+        if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
+        return getCursorPosition(rows, cols);
     }
 
     *cols = ws.ws_col;
@@ -108,7 +128,7 @@ int getWindowSize(int *rows, int *cols) {
  */
 void editorDrawRows() {
     int y;
-    for (y = 0; y < 24; y++) {
+    for (y = 0; y < E.screenRows; y++) {
         write(STDOUT_FILENO, "~\r\n", 3);
     }
 }

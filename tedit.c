aa505629a530c49e06fc276022ce57b4c300a7d4
dingdongg <cs.dg.kim@gmail.com>
2024-06-28 20:21:14 -0600

    add delete key detection

--- tedit.c
+++ tedit.c
@@ -25,6 +25,7 @@ enum editorKey {
     PAGE_DOWN,
     HOME_KEY,
     END_KEY,
+    DEL_KEY,
 };
 
 // DATA //
@@ -115,6 +116,7 @@ int editorReadKey() {
                 if (seq[2] == '~') {
                     switch (seq[1]) {
                         case '1': return HOME_KEY;
+                        case '3': return DEL_KEY;
                         case '4': return END_KEY;
                         case '5': return PAGE_UP;
                         case '6': return PAGE_DOWN;
